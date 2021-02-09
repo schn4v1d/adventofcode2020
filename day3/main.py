eb51a590dfe7f5ee6f41b90dd2e933e7c6d7b308
David Schneider <voxldavid@gmail.com>
2021-02-09 11:57:34 +0100

    Day 3 prettier

--- day3/main.py
+++ day3/main.py
@@ -1,4 +1,5 @@
 def slope(xstep, ystep, lines, width):
+    width = len(lines[0])
     trees = 0
     for y in range(0, len(lines), ystep):
         x = (y // ystep) * xstep
@@ -10,10 +11,13 @@ def main():
     input = open("input")
     lines = list(map(lambda a: a.strip(), input.readlines()))
     input.close()
-    width = len(lines[0])
-    part_one = slope(3, 1, lines, width)
+    part_one = slope(3, 1, lines)
     print(f"--- Part One ---\n\nTrees: {part_one}\n")
-    part_two = slope(1, 1, lines, width) * slope(3, 1, lines, width) * slope(5, 1, lines, width) * slope(7, 1, lines, width) * slope(1, 2, lines, width)
+    part_two = slope(1, 1, lines)\
+             * slope(3, 1, lines)\
+             * slope(5, 1, lines)\
+             * slope(7, 1, lines)\
+             * slope(1, 2, lines)
     print(f"--- Part Two ---\n\nTrees: {part_two}\n")
 
 if __name__ == "__main__":
